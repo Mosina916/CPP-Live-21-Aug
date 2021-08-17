5106ca26da5618292f04174721bc8706aa3a59b3
mosina916 <mosinaashraf1913@gmail.com>
2021-08-17 18:26:12 +0530

    Lecture 1

---
+++ Lecture1/conditionals.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+int main(){ //starting point 
+	// bool israining=true;
+
+	// int c=1;
+
+
+
+
+
+	// // conditionals
+	// // if(condition-->non zero value){
+	// // 	// condition -->true
+	// // }
+
+
+	// if(israining==true){
+	// 	cout<<"yes take umbrella"<<endl;
+	// }
+	// else{
+	// 	cout<<"don't take umbrella"<<endl;
+	// }
+	
+
+	// int age=69;
+	// if(69<=50){
+	// 	cout<<"take car"<<endl;
+	// }
+	// else{
+
+	// 	cout<<"by foot"<<endl;
+
+	// }
+
+
+	int stu=66;
+	if(1 and stu<=100){
+		cout<<"Grade A"<<endl;
+	}
+	else if(stu>=70&&stu<=89){
+		cout<<"Grade B"<<endl;
+	}
+	else if(stu>=60&&stu<=69){
+		cout<<"Grade C"<<endl;
+	}
+	else if(stu>=50&&stu<=59){
+		cout<<"Grade D"<<endl;
+
+	}
+	// else{
+	// 	cout<<"Fail"<<endl;
+	// }
+
+
+
+
+	int t=1213;
+	int h=143;
+	if(t==1213){
+		cout<<"t is 1213"<<endl;
+	}
+
+	if(h<=14){
+		cout<<"h is greater than 12234"<<endl;
+
+	}
+
+
+
+
+
+	
+
+
+
+
+
+	return 0; //terminate successfully
+
+
+}
---
+++ Lecture1/hello.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+int main(){ //starting point 
+	// int s1; //declare
+	// s1=10; //assignment
+
+	// to print -->cout-->console output
+	// cout<<s1<<'\n';
+
+	// // '\n'-->endl-->next line
+
+	// cout<<60<<endl;
+
+	// // string -->" "
+
+	// cout<<"Hello world"<<endl;
+	// cout<<'A'<<endl;
+
+
+	// cout<<s1<<'\n'<<60<<'\n'<<"hello world"<<endl<<'A'<<endl;
+
+	// float f=20.3456; //assignmnet
+	// cout<<f<<endl;
+
+
+	// // user input
+	// int x;
+	// cin>>x; 
+
+	// cout<<x<<endl;
+
+	// A-Z or a-z or _ 
+
+
+	bool israining=false; //true-->1 false-->0 bool-->1 byte
+
+	cout<<israining<<endl;
+
+
+	// int s1=10; //10 40 
+	// int s2=40;
+	// int s3=89;
+	// int s4=45;
+
+	int s1=10,s2=40,s3=89,s4=45;
+
+
+
+	cout<<s1<<" "<<s2<<" "<<s3<<" "<<s4<<endl;
+
+
+
+
+
+
+
+
+	// task
+	
+
+	return 0; //terminate successfully
+
+
+	cout<<"this is coding blocks"<<endl;
+	cout<<"coding blocks"<<endl;
+
+}
