e8b9b3ece8b34047eedb4e5f6d04530ccbe467a5
Mosina916 <mosinaashraf43@gmail.com>
2021-10-20 02:06:54 +0530

    Lecture 23 ll

--- Lecture23/test.cpp
+++ Lecture23/test.cpp
@@ -35,17 +35,108 @@ void insertionathead(node*&head,node*&tail,int data){
 
 }
 
-void printll(node*head){
-	node*temp=head;
 
-// loop
-	while(temp!=NULL){
-			cout<<temp->data<<"--> ";//30 50 80
-			temp=temp->next;
+
+
+void printll(node*head){
+// 	node*temp=head;
+
+// // loop
+// 	while(temp!=NULL){
+// 			cout<<temp->data;//30-->50-->80-->
+// 			temp=temp->next;
+
+// 	}
+
+// 	cout<<endl;
+
+
+
+// LOOP
+	while(head!=NULL){
+		cout<<head->data<<"--> ";//30 50 80
+	head=head->next;
+
+
 
 	}
-
 	cout<<endl;
+	
+
+
+}
+
+
+void insertattail(node*&head,node*&tail,int data){
+	node*n=new node(data);
+
+	// 1 ll empty
+	if(head==NULL){
+		head=tail=n;
+	}
+
+	// 2. ll is not empty
+
+	else{
+		tail->next=n;
+		tail=n;
+	}
+
+}
+int length(node*head){
+	int cnt=0;
+	node*temp=head;
+	while(temp!=NULL){
+		cnt++;
+		temp=temp->next;
+	}
+
+
+	return cnt;
+
+}
+
+
+int lenrec(node*head){
+
+	// base case
+	if(head==NULL){
+		return 0;
+	}
+
+
+	// recusrive 
+
+	return 1+lenrec(head->next);
+
+}
+
+void insertatanypost(node*&head,node*&tail,int data,int pos){
+
+	if(pos==0){
+		insertionathead(head,tail,data);
+	}
+	else if(pos>=length(head)){
+		insertattail(head,tail,data);
+
+	}
+	else{
+		node*n=new node(data);
+		node*temp=head;
+
+		for(int jump=1;jump<pos;jump++){
+			temp=temp->next;
+		}
+
+
+
+
+
+
+
+		n->next=temp->next;
+		temp->next=n;
+	}
 
 
 }
@@ -75,8 +166,25 @@ int main(){
 
 	printll(head);
 
+	// printll(head);
+
+
+
+	insertattail(head,tail,70);
+	insertattail(head,tail,500);
+
+
 	printll(head);
 
+	int pos;
+	cin>>pos;//
+
+	insertatanypost(head,tail,65,pos);
+
+	printll(head);
+
+
+cout<<"Length of ll is "<<lenrec(head)<<endl;
 	// n1+n2;
 
 
